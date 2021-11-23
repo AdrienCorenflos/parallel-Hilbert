7f09db978c1ee1747402913b5851172d1f3283de
adriencorenflos <adrien.corenflos@gmail.com>
2021-11-23 11:34:27 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='parallel-Hilbert',
+    version='0.0.1',
+    description='JAX-based Hilbert sorting.',
+    author='Adrien Corenflos',
+    author_email='adrien.corenflos@gmail.com',
+    url='https://github.com/AdrienCorenflos/parallel-Hilbert',
+)
