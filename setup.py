5282afc9a881dbf192fe3b9c6d98d5819b32dac4
adriencorenflos <adrien.corenflos@gmail.com>
2021-11-23 11:47:01 +0200

    Change

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     description='JAX-based Hilbert sorting.',
     author='Adrien Corenflos',
+    package_name="hilbert_sort",
     author_email='adrien.corenflos@gmail.com',
     url='https://github.com/AdrienCorenflos/parallel-Hilbert',
 )
